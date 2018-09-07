7920572c3e26b3701e4122d069392fbf390411f1
Daniel Ridley <ridleydan31@gmail.com>
2018-09-07 18:55:02 -0500

    added math operator

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,21 +5,35 @@ $(document).ready(readyNow);
 function readyNow() {
     console.log('JQ');
     $('.operators').on('click', getOperator);
-    $('#equalsButton').on('click', doCalculations)
-
+    $('#equalsButton').on('click', equalsPackage)
+    doCalculations();
 }
+
 let operation;
+let objectArray = []
+//this will create functionality when the user clicks on any of the operator buttons
+
 function getOperator() {
     console.log('operator working');
     operation = $(this).text()
 }
 
-function doCalculations() {
+function equalsPackage() {
     let objectToSend = {
         number1: $('#firstNumber').val(),
         number2: $('#lastNumber').val(),
         operation: operation
     }
-   console.log(objectToSend);
+    console.log(objectToSend);
     
+}
+function doCalculations (){
+    let num1 = $('#firstNumber').val()
+    let num2 = $('#lastNumber').val()
+    if (operation == '+' ) {
+        answer = num1 + num2;
+        return answer;  
+        console.log(answer);
+        
+    }
 }
