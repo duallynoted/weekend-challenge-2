1d225d86c97f96529029297ba3813a6d9448234d
Daniel Ridley <ridleydan31@gmail.com>
2018-09-08 16:12:40 -0500

    successfully completing operations on server side and pushing into array

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -24,26 +24,36 @@ function equalsPackage() {
         operation: operation
     }
     console.log(objectToSend);
-    doCalculations();
+    // doCalculations();
     $('#operatorOutput').empty();
     $('#operatorOutput').append('Your Calculation: ' + totals);
+    $.ajax({
+        method: 'POST',
+        url:'/calculations',
+        data: objectToSend
+    }).then(function (response) {
+        console.log('back from POST with: ', response);        
+
+    }).catch (function(error){
+
+    })
 }
 
-function doCalculations (){
-    let num1 = $('#firstNumber').val()
-    let num2 = $('#lastNumber').val()
-    // let answer = 0;
-    if (operation == '+' ) {
-        answer = parseInt(num1) + parseInt(num2);
-        console.log('add:', answer);          
-    } else if (operation == '-' ) {
-        answer = parseInt(num1) - parseInt(num2);
-        console.log(answer);          
-    } else if (operation == '*' ) {
-        answer = parseInt(num1) * parseInt(num2);
-        console.log(answer);
-    } else if (operation == '/' ) {
-        answer = parseInt(num1) / parseInt(num2);
-        console.log(answer);
-} totals.push(answer);
-};  
+// function doCalculations (){
+//     let num1 = $('#firstNumber').val()
+//     let num2 = $('#lastNumber').val()
+//     // let answer = 0;
+//     if (operation == '+' ) {
+//         answer = parseInt(num1) + parseInt(num2);
+//         console.log('add:', answer);          
+//     } else if (operation == '-' ) {
+//         answer = parseInt(num1) - parseInt(num2);
+//         console.log(answer);          
+//     } else if (operation == '*' ) {
+//         answer = parseInt(num1) * parseInt(num2);
+//         console.log(answer);
+//     } else if (operation == '/' ) {
+//         answer = parseInt(num1) / parseInt(num2);
+//         console.log(answer);
+// } totals.push(answer);
+// };  
--- server/server.js
+++ server/server.js
@@ -5,16 +5,38 @@ const app = express();
 
 //uses
 app.use(express.static('server/public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //globals
 const PORT = 5000;
+let totals= [];
 
 //spin up server
-app.listen(PORT, ()=>{
-    console.log('SERVER IS UP ON: ', PORT);    
+app.listen(PORT, () => {
+    console.log('SERVER IS UP ON: ', PORT);
 })
-app.post('/', (req, res)=>{
+app.post('/calculations', (req, res) => {
     console.log('in / POST', req.body);
-    res.send(handleAddButton);
+    doCalculations(req.body.number1, req.body.number2, req.body.operation);
+    res.send(totals);
 })
+
+function doCalculations(num1, num2, operation) {
+    if (operation == '+') {
+        answer = parseInt(num1) + parseInt(num2);
+        console.log('add:', answer);
+        totals.push(answer);
+    } else if (operation == '-') {
+        answer = parseInt(num1) - parseInt(num2);
+        console.log(answer);
+        totals.push(answer);
+    } else if (operation == '*') {
+        answer = parseInt(num1) * parseInt(num2);
+        console.log(answer);
+        totals.push(answer);
+    } else if (operation == '/') {
+        answer = parseInt(num1) / parseInt(num2);
+        console.log(answer);
+        totals.push(answer);
+    };
+}; 
